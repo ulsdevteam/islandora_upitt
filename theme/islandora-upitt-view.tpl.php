1bac2b6dd7a772a215ec8d3dba7431580134d0eb
Brian Gillingham <bgilling@pitt.edu>
2016-09-12 12:10:47 -0400

    renamed sidebar-right to sidebar

--- theme/islandora-upitt-view.tpl.php
+++ theme/islandora-upitt-view.tpl.php
@@ -1,15 +1,15 @@
 <?php
 /**
-	* @file
-	* Islandora-upitt-view display template.
-	*
-	* Variables available:
-	* - $upitt_date: The defined date corresponding to the Solr field.
-	* - $upitt_creator: The defined creator corresponding to the Solr field.
-	* - $upitt_lg_thumb: The defined large thumbnail image corresponding to the Solr field.
-	*
-	* @see template_preprocess_islandora-upitt-view
-	*/
+        * @file
+        * Islandora-upitt-view display template.
+        *
+        * Variables available:
+        * - $upitt_date: The defined date corresponding to the Solr field.
+        * - $upitt_creator: The defined creator corresponding to the Solr field.
+        * - $upitt_lg_thumb: The defined large thumbnail image corresponding to the Solr field.
+        *
+        * @see template_preprocess_islandora-upitt-view
+        */
 ?>
 
 <div id="two-col-left-main">
@@ -17,12 +17,10 @@
   <p class="subtitle"><?php print $upitt_creator; ?></p>
   <p class="subtitle"><?php print $upitt_viewer_link; ?></p>
   <div id="main-content"> 
-  <?php if (isset($upitt_object_view)) { 
-    print $upitt_object_view; 
-  } ?> 
-  <?php print $upitt_lg_thumb; ?> 
+    <?php print $upitt_object_view; ?> 
+    <?php print $upitt_lg_thumb; ?> 
   </div><!-- /end main-content -->
-  <div id="sidebar-right">
+  <div id="sidebar"> 
     <?php print render($page['sidebar']); ?>
     <?php print $metadata; ?>
   </div><!-- /end sidebar -->
