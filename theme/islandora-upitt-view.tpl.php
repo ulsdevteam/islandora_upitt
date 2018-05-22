a317971ba4a991f3434a9fa84317396498919126
Brian Gillingham <bgilling@pitt.edu>
2018-05-22 10:35:19 -0400

    Added configure options for and handling for displaying datastream info - as well as to prevent download links based on copyright status (alter_menu - access updated as well)

--- theme/islandora-upitt-view.tpl.php
+++ theme/islandora-upitt-view.tpl.php
@@ -23,10 +23,31 @@
     <?php print $upitt_object_view; ?> 
     <?php endif; ?>
     <?php print $upitt_lg_thumb; ?> 
+
   </div><!-- /end main-content -->
   <div id="sidebar"> 
     <?php print render($page['sidebar']); ?>
     <?php print $metadata; ?>
     <?php print $discovery_links_block; ?>
   </div><!-- /end sidebar -->
+
+  <fieldset class="file_details">
+    <legend><span class="fieldset-legend"><?php print t('Resources'); ?></span></legend>
+    <table>
+      <tr>
+        <th><?php print t('Resource Label'); ?></th>
+        <th class="right_align"><?php print t('Size'); ?></th>
+      </tr>
+      <?php foreach($datastreams as $key => $value): ?>
+      <tr>
+          <td><?php if ($value['download_prohibit_reasons']) : ?>
+              <span title="<?php print $value['download_prohibit_reasons']; ?>"><?php print $value['label']; ?></span>
+              <?php elseif(isset($value['label_link'])): ?><?php print $value['label_link']; ?><?php endif; ?>
+          </td>
+          <td class="right_align"><?php if(isset($value['size'])): ?><?php print $value['size']; ?><?php endif; ?></td>
+      </tr>
+      <?php endforeach; ?>
+    </table>
+  </fieldset>
+
 </div><!-- /end two-col-left-main -->
